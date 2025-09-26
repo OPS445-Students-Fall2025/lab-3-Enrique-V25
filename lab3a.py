d3d63f90cef48acd681733d2351f8add5741ddd3
Enrique Molina <emolina3@myseneca.ca>
2025-09-26 20:59:32 +0000

    Finshed lab3a lab3b lab3c

---
+++ lab3a.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# return_text_value() function
+#Author ID: emolina3
+
+def return_text_value():
+   name = 'Terry'
+   Greeting = 'Good Morning ' + name
+   return Greeting
+
+
+
+# return_number_value() function
+
+def return_number_value(): 
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+  
+
+    
+# Main Program
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
+import lab3a
+text = lab3a.return_text_value()
+print(text)
+print(lab3a.return_text_value())
+
