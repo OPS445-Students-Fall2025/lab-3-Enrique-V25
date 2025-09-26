5e7d214ef81f9190a2f8db9005223ac28ea4254c
Enrique Molina <emolina3@myseneca.ca>
2025-09-26 21:18:42 +0000

    Finished a b c d

--- lab3d.py
+++ lab3d.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 #Author ID: emolina3
 
+import os 
+
+def free_space():
+    freeSpace = os.popen("df -h | grep '/$' | awk '{print $4}'").read().strip()
+    return freeSpace
+print(free_space())
